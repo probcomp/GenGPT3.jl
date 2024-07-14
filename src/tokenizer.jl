ab74c73aa2a877d044d7c1327cc4cdc6c0a3930e
Xuan <tanqazx@gmail.com>
2024-07-14 18:32:28 -0400

    Ensure compat with BytePairEncoding 0.5.2.

--- src/tokenizer.jl
+++ src/tokenizer.jl
@@ -3,7 +3,7 @@
 using TextEncodeBase
 using BytePairEncoding
 
-using TextEncodeBase: AbstractTokenization
+using TextEncodeBase: AbstractTokenization, DATLookupDict, lookup_word
 using BytePairEncoding: BPETokenizer, BPETokenization, TikTokenBPE, StringView
 
 const CL100K_BASE = BytePairEncoding.load_tiktoken("cl100k_base")
@@ -16,21 +16,9 @@ const TOKENIZERS = Dict(
     "r50k_base" => R50K_BASE
 )
 
-const ENCODERS = Dict{String, Dict{Vector{UInt8}, Int}}()
-for (name, tkr) in TOKENIZERS
-    bpe = tkr.tokenization.base.bpe
-    ENCODERS[name] = bpe.encoder
-end
-
-const DECODERS = Dict{String, Dict{Int, Vector{UInt8}}}()
-for (name, tkr) in TOKENIZERS
-    bpe = tkr.tokenization.base.bpe
-    decoder = Dict{Int, Vector{UInt8}}()
-    for (code, id) in bpe.encoder
-        decoder[id] = code
-    end
-    DECODERS[name] = decoder
-end
+const ENCODERS = Dict(
+    name => tkr.tokenization.base.bpe.encoder for (name, tkr) in TOKENIZERS
+)
 
 const EOT_IDS = Dict(
     "cl100k_base" => 100257,
@@ -38,7 +26,7 @@ const EOT_IDS = Dict(
     "r50k_base" => 50256
 )
 
-const SPECIAL_TOKENS = Dict(
+const SPECIAL_TOKENS_ENCODER = Dict(
     "cl100k_base" => Dict(
         "<|endoftext|>" => 100257,
         "<|fim_prefix|>" => 100258,
@@ -53,12 +41,11 @@ const SPECIAL_TOKENS = Dict(
         "<|endoftext|>" => 50256
     )
 )
-for (name, tokens) in SPECIAL_TOKENS
-    decoder = DECODERS[name]
-    for (token, id) in tokens
-        decoder[id] = codeunits(token)
-    end
-end
+
+const SPECIAL_TOKENS_DECODER = Dict(
+    name => Dict(id => str for (str, id) in tokens)
+    for (name, tokens) in SPECIAL_TOKENS_ENCODER
+)
 
 const MODEL_ENCODINGS = Dict(
     "gpt-4" => "cl100k_base",
@@ -95,8 +82,8 @@ for `encoding` include `"cl100k_base"`, `"p50k_base"` and `"r50k_base"`.
 """
 function encode(name::AbstractString, tokens::AbstractVector{<:AbstractString})
     encoder = ENCODERS[name]
-    special = SPECIAL_TOKENS[name]
-    return map(t -> get(() -> encoder[codeunits(t)], special, t), tokens)
+    special = SPECIAL_TOKENS_ENCODER[name]
+    return map(t -> get(() -> get(encoder, t, -1) - 1, special, t), tokens)
 end
 encode(t::BPETokenizer, tokens::AbstractVector{<:AbstractString}) =
     encode(TextEncodeBase.tokenization(t), tokens)
@@ -105,7 +92,7 @@ encode(t::AbstractTokenization, tokens::AbstractVector{<:AbstractString}) =
 encode(t::BPETokenization, tokens::AbstractVector{<:AbstractString}) =
     encode(t.bpe, tokens)
 encode(bpe::TikTokenBPE, tokens::AbstractVector{<:AbstractString}) =
-    map(t -> bpe.encoder[codeunits(t)], tokens)
+    map(t -> get(bpe.encoder, t, -1), tokens)
 
 """
     decode(encoding, token_ids)
@@ -114,8 +101,13 @@ Decodes a sequence of token IDs into string tokens. Supported values for
 `encoding` include `"cl100k_base"`, `"p50k_base"` and `"r50k_base"`.
 """
 function decode(name::AbstractString, token_ids::AbstractVector{<:Integer})
-    decoder = DECODERS[name]
-    return map(id -> String(StringView(decoder[id])), token_ids)
+    encoder = ENCODERS[name]
+    special = SPECIAL_TOKENS_DECODER[name]
+    return map(token_ids) do id
+        get(special, id) do 
+            TextEncodeBase.lookup_word(encoder, "", id + 1)
+        end
+    end
 end
 
 """
